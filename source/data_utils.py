be29257af1995cac2986bacc5299858bed66975f
Naira Mnatsakanyan <naira.mnatsakanyan@yahoo.com>
2024-05-24 09:13:21 +0400

    Create Separate File For CustomDataset class

--- source/data_utils.py
+++ source/data_utils.py
@@ -1,27 +1,7 @@
-from PIL import Image
-
 import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 
-
-class CustomDataset(Dataset):
-    def __init__(self, image_paths, labels, transform=None):
-        self.image_paths = image_paths
-        self.labels = labels
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
-        img = Image.open(img_path).convert('RGB')
-        label = self.labels[idx]
-
-        if self.transform:
-            img = self.transform(img)
-
-        return img, label
+from CustomDataset import CustomDataset
 
 
 def preprocess_data(train=True):
